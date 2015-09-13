b3f262f48a438f1668a7fa85e63de94e796d1c62
Shintaro Katafuchi <hot.chemistry21@gmail.com>
2015-09-14 00:58:54 +0900

    Delete unnecesarry test case.

--- permissionsdispatcher-processor/src/test/java/permissions/dispatcher/processor/ClassTypeTest.java
+++ permissionsdispatcher-processor/src/test/java/permissions/dispatcher/processor/ClassTypeTest.java
@@ -2,8 +2,6 @@ package permissions.dispatcher.processor;
 
 import org.junit.Test;
 
-import jdk.nashorn.internal.ir.annotations.Ignore;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -11,29 +9,10 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class ClassTypeTest {
 
-    private static final TypeResolver MOCK_RESOLVER = new TypeResolver() {
-        @Override
-        public boolean isSubTypeOf(String subTypeClass, String superTypeClass) {
-            String superTypeWithoutPackage = superTypeClass.substring(superTypeClass.lastIndexOf('.') + 1);
-            return subTypeClass.contains(superTypeWithoutPackage);
-        }
-    };
-
     @Test
     public void getActivityTest() {
         assertThat(ClassType.ACTIVITY.getActivity()).isEqualTo("target");
         assertThat(ClassType.FRAGMENT.getActivity()).isEqualTo("target.getActivity()");
-    }
-
-    @Test
-    @Ignore
-    public void getClassTypeTest() {
-        assertThat(ClassType.getClassType("MainActivity", MOCK_RESOLVER)).isEqualTo(ClassType.ACTIVITY);
-        assertThat(ClassType.getClassType("MainFragment", MOCK_RESOLVER)).isEqualTo(ClassType.FRAGMENT);
-        assertThat(ClassType.getClassType("ActivityMain", MOCK_RESOLVER)).isEqualTo(ClassType.ACTIVITY);
-        assertThat(ClassType.getClassType("FragmentMain", MOCK_RESOLVER)).isEqualTo(ClassType.FRAGMENT);
-        assertThat(ClassType.getClassType("MyService", MOCK_RESOLVER)).isNull();
-        assertThat(ClassType.getClassType("SomeBroadcastReceiver", MOCK_RESOLVER)).isNull();
     }
 
 }
