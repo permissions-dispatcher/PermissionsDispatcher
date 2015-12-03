3c7900f28ce67b2ec6dfe23496a5ebe705cc0eeb
marcel <aurae@web.de>
2015-12-03 21:58:39 +0100

    Added convenience method to redirect a user to the app's Settings to PermissionUtils

--- library/src/main/android/permissions/dispatcher/PermissionUtils.java
+++ library/src/main/android/permissions/dispatcher/PermissionUtils.java
@@ -2,9 +2,16 @@ package permissions.dispatcher;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 
+import static android.os.Build.VERSION_CODES.FROYO;
+import static android.os.Build.VERSION_CODES.GINGERBREAD;
 import static android.support.v4.content.PermissionChecker.checkSelfPermission;
 
 public final class PermissionUtils {
@@ -30,7 +37,7 @@ public final class PermissionUtils {
     /**
      * Returns true if the Activity or Fragment has access to all given permissions.
      *
-     * @param context context
+     * @param context     context
      * @param permissions permission list
      * @return returns true if the Activity or Fragment has access to all given permissions.
      */
@@ -57,5 +64,34 @@ public final class PermissionUtils {
             }
         }
         return false;
+    }
+
+    /**
+     * Creates an {@link Intent} that redirects the user to the app's settings page.
+     *
+     * @param context Context used to access the app's package name
+     * @return An intent that, when used to {@link Activity#startActivity(Intent)}, will open the running app's Settings page.
+     * If no matching Activity could be found for the device's settings, this method returns null.
+     */
+    public static @Nullable Intent createAppSettingsIntent(Context context) {
+        Intent intent;
+        String packageName = context.getPackageName();
+        if (Build.VERSION.SDK_INT >= GINGERBREAD) {
+            // Utilize the dedicated Settings Action on API 9+
+            intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", packageName, null);
+            intent.setData(uri);
+
+        } else {
+            // Compatibility implementation using a View intent and the native Settings app
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
+            String extraKey = Build.VERSION.SDK_INT == FROYO ? "pkg" : "com.android.settings.ApplicationPkgName";
+            intent.putExtra(extraKey, packageName);
+        }
+
+        // If the intent can be used to resolve an Activity, return that intent. Otherwise, return null
+        return context.getPackageManager().resolveActivity(intent, 0) != null ? intent : null;
     }
 }
