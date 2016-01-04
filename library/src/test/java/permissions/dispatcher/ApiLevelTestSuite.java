e5d9090726525f4c6dd5a2359a3eebc9d5abf2f8
Shintaro Katafuchi <hot.chemistry21@gmail.com>
2016-01-05 06:45:45 +0900

    Fix tiny stuff and apply formatter.

--- library/src/test/java/permissions/dispatcher/ApiLevelTestSuite.java
+++ library/src/test/java/permissions/dispatcher/ApiLevelTestSuite.java
@@ -30,120 +30,129 @@ import static org.mockito.Matchers.anyString;
  * Test suite related to permissions that were added to Android at a later point in the platform's lifecycle.
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({ PermissionChecker.class })
+@PrepareForTest({PermissionChecker.class})
 @SuppressLint("NewApi")
 public class ApiLevelTestSuite {
 
-	private static final int MOST_RECENT_API_LEVEL = Build.VERSION_CODES.M;
-	private static final int NEEDS_PERMISSION_CHECK = 1024;
+    private static final int MOST_RECENT_API_LEVEL = Build.VERSION_CODES.M;
+    private static final int NEEDS_PERMISSION_CHECK = 1024;
 
-	private final Context mockContext;
+    private final Context mockContext;
 
-	public ApiLevelTestSuite() {
-		// Mock out Context
-		mockContext = Mockito.mock(Context.class);
-	}
+    public ApiLevelTestSuite() {
+        // Mock out Context
+        mockContext = Mockito.mock(Context.class);
+    }
 
-	@Before public void beforeTest() throws Exception {
-		// Reset the API level assumption
-		this.resetApiLevel();
+    @Before
+    public void beforeTest() throws Exception {
+        // Reset the API level assumption
+        this.resetApiLevel();
 
-		// Mock out PermissionChecker, so that "checkSelfPermission"
-		// always returns NEEDS_PERMISSION_CHECK.
-		// This way, we can distinguish between auto-grants and permission-checks
-		PowerMockito.mockStatic(PermissionChecker.class);
-		BDDMockito.given(PermissionChecker.checkSelfPermission(any(Context.class), anyString())).willReturn(NEEDS_PERMISSION_CHECK);
-	}
+        // Mock out PermissionChecker, so that "checkSelfPermission"
+        // always returns NEEDS_PERMISSION_CHECK.
+        // This way, we can distinguish between auto-grants and permission-checks
+        PowerMockito.mockStatic(PermissionChecker.class);
+        BDDMockito.given(PermissionChecker.checkSelfPermission(any(Context.class), anyString())).willReturn(NEEDS_PERMISSION_CHECK);
+    }
 
-	@Test public void testAssumeApiLevelWorking() throws Exception {
-		// Check that manually setting the API level to a value works
-		assumeApiLevel(ICE_CREAM_SANDWICH);
-		assertEquals(ICE_CREAM_SANDWICH, Build.VERSION.SDK_INT);
+    @Test
+    public void testAssumeApiLevelWorking() throws Exception {
+        // Check that manually setting the API level to a value works
+        assumeApiLevel(ICE_CREAM_SANDWICH);
+        assertEquals(ICE_CREAM_SANDWICH, Build.VERSION.SDK_INT);
 
-		// Check that resetting the API level works
-		resetApiLevel();
-		assertEquals(0, Build.VERSION.SDK_INT);
-	}
+        // Check that resetting the API level works
+        resetApiLevel();
+        assertEquals(0, Build.VERSION.SDK_INT);
+    }
 
-	@Test public void testCheckSelfPermissionMockWorking() throws Exception {
-		// Check that mocking out PermissionChecker works
-		assertEquals(NEEDS_PERMISSION_CHECK, PermissionChecker.checkSelfPermission(mockContext, "permission"));
-	}
+    @Test
+    public void testCheckSelfPermissionMockWorking() throws Exception {
+        // Check that mocking out PermissionChecker works
+        assertEquals(NEEDS_PERMISSION_CHECK, PermissionChecker.checkSelfPermission(mockContext, "permission"));
+    }
 
-	@Test public void testAddVoicemailPermission() throws Exception {
-		// ADD_VOICEMAIL:
-		// Added in API level 14 ("Ice Cream Sandwich")
-		iteratePermissionCheck(Manifest.permission.ADD_VOICEMAIL, ICE_CREAM_SANDWICH);
-	}
+    @Test
+    public void testAddVoicemailPermission() throws Exception {
+        // ADD_VOICEMAIL:
+        // Added in API level 14 ("Ice Cream Sandwich")
+        iteratePermissionCheck(Manifest.permission.ADD_VOICEMAIL, ICE_CREAM_SANDWICH);
+    }
 
-	@Test public void testBodySensorsPermission() throws Exception {
-		// BODY_SENSORS:
-		// Added in API level 20 ("KitKat Watch")
-		iteratePermissionCheck(Manifest.permission.BODY_SENSORS, KITKAT_WATCH);
-	}
+    @Test
+    public void testBodySensorsPermission() throws Exception {
+        // BODY_SENSORS:
+        // Added in API level 20 ("KitKat Watch")
+        iteratePermissionCheck(Manifest.permission.BODY_SENSORS, KITKAT_WATCH);
+    }
 
-	@Test public void testReadCallLogPermission() throws Exception {
-		// READ_CALL_LOG:
-		// Added in API level 16 ("Jelly Bean")
-		iteratePermissionCheck(Manifest.permission.READ_CALL_LOG, JELLY_BEAN);
-	}
+    @Test
+    public void testReadCallLogPermission() throws Exception {
+        // READ_CALL_LOG:
+        // Added in API level 16 ("Jelly Bean")
+        iteratePermissionCheck(Manifest.permission.READ_CALL_LOG, JELLY_BEAN);
+    }
 
-	@Test public void testReadExternalStoragePermission() throws Exception {
-		// READ_EXTERNAL_STORAGE:
-		// Added in API level 16 ("Jelly Bean")
-		iteratePermissionCheck(Manifest.permission.READ_EXTERNAL_STORAGE, JELLY_BEAN);
-	}
+    @Test
+    public void testReadExternalStoragePermission() throws Exception {
+        // READ_EXTERNAL_STORAGE:
+        // Added in API level 16 ("Jelly Bean")
+        iteratePermissionCheck(Manifest.permission.READ_EXTERNAL_STORAGE, JELLY_BEAN);
+    }
 
-	@Test public void testUseSipPermission() throws Exception {
-		// USE_SIP:
-		// Added in API level 9 ("Gingerbread")
-		iteratePermissionCheck(Manifest.permission.USE_SIP, GINGERBREAD);
-	}
+    @Test
+    public void testUseSipPermission() throws Exception {
+        // USE_SIP:
+        // Added in API level 9 ("Gingerbread")
+        iteratePermissionCheck(Manifest.permission.USE_SIP, GINGERBREAD);
+    }
 
-	@Test public void testWriteCallLogPermission() throws Exception {
-		// WRITE_CALL_LOG:
-		// Added in API level 16 ("Jelly Bean")
-		iteratePermissionCheck(Manifest.permission.WRITE_CALL_LOG, JELLY_BEAN);
-	}
+    @Test
+    public void testWriteCallLogPermission() throws Exception {
+        // WRITE_CALL_LOG:
+        // Added in API level 16 ("Jelly Bean")
+        iteratePermissionCheck(Manifest.permission.WRITE_CALL_LOG, JELLY_BEAN);
+    }
 
 	/* Begin private */
 
-	private void iteratePermissionCheck(String permission, int permissionMinLevel) throws Exception {
-		for (int apiLevel = 0; apiLevel <= MOST_RECENT_API_LEVEL; apiLevel++) {
-			// Adjust the current API level
-			assumeApiLevel(apiLevel);
+    private void iteratePermissionCheck(String permission, int permissionMinLevel) throws Exception {
+        for (int apiLevel = 0; apiLevel <= MOST_RECENT_API_LEVEL; apiLevel++) {
+            // Adjust the current API level
+            assumeApiLevel(apiLevel);
 
-			// Iterate over all API levels and verify that the permission is auto-granted
-			// below the minimum available level. For all other API levels, the permission
-			// shouldn't be auto-granted.
-			boolean shouldAutoGrantPermission = apiLevel < permissionMinLevel;
-			boolean hasPermission = PermissionUtils.hasSelfPermissions(mockContext, permission);
+            // Iterate over all API levels and verify that the permission is auto-granted
+            // below the minimum available level. For all other API levels, the permission
+            // shouldn't be auto-granted.
+            boolean shouldAutoGrantPermission = apiLevel < permissionMinLevel;
+            boolean hasPermission = PermissionUtils.hasSelfPermissions(mockContext, permission);
 
-			if (shouldAutoGrantPermission != hasPermission) {
-				// Mismatch, because the permission shouldn't be granted because the API level requires a check,
-				// OR the permission should be granted because it doesn't exist on the current API level
-				throw new AssertionError(permission + " check on API level " + apiLevel + " shouldn't return auto-grant=" + shouldAutoGrantPermission + " amd has-permission=" + hasPermission);
-			}
-		}
-	}
+            if (shouldAutoGrantPermission != hasPermission) {
+                // Mismatch, because the permission shouldn't be granted because the API level requires a check,
+                // OR the permission should be granted because it doesn't exist on the current API level
+                throw new AssertionError(permission + " check on API level " + apiLevel + " shouldn't return auto-grant=" + shouldAutoGrantPermission + " amd has-permission=" + hasPermission);
+            }
+        }
+    }
 
-	private void assumeApiLevel(int apiLevel) throws Exception {
-		// Adjust the value of Build.VERSION.SDK_INT statically using reflection
-		Field sdkIntField = Build.VERSION.class.getDeclaredField("SDK_INT");
-		sdkIntField.setAccessible(true);
+    private void assumeApiLevel(int apiLevel) throws Exception {
+        // Adjust the value of Build.VERSION.SDK_INT statically using reflection
+        Field sdkIntField = Build.VERSION.class.getDeclaredField("SDK_INT");
+        sdkIntField.setAccessible(true);
 
-		// Temporarily remove the SDK_INT's "final" modifier
-		Field modifiersField = Field.class.getDeclaredField("modifiers");
-		modifiersField.setAccessible(true);
-		modifiersField.setInt(sdkIntField, sdkIntField.getModifiers() & ~Modifier.FINAL);
+        // Temporarily remove the SDK_INT's "final" modifier
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(sdkIntField, sdkIntField.getModifiers() & ~Modifier.FINAL);
 
-		// Update the SDK_INT value, re-finalize the field, and lock it again
-		sdkIntField.set(null, apiLevel);
-		modifiersField.setInt(sdkIntField, sdkIntField.getModifiers() | Modifier.FINAL);
-		sdkIntField.setAccessible(false);
-	}
+        // Update the SDK_INT value, re-finalize the field, and lock it again
+        sdkIntField.set(null, apiLevel);
+        modifiersField.setInt(sdkIntField, sdkIntField.getModifiers() | Modifier.FINAL);
+        sdkIntField.setAccessible(false);
+    }
 
-	private void resetApiLevel() throws Exception {
-		this.assumeApiLevel(0);
-	}
+    private void resetApiLevel() throws Exception {
+        this.assumeApiLevel(0);
+    }
 }
