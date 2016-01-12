bb2537c91dac8287dbc24839033d8986980792b4
Shintaro Katafuchi <hot.chemistry21@gmail.com>
2016-01-13 06:40:11 +0900

    Delete unused method and update dependency.

--- library/src/main/android/permissions/dispatcher/PermissionUtils.java
+++ library/src/main/android/permissions/dispatcher/PermissionUtils.java
@@ -2,12 +2,8 @@ package permissions.dispatcher;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
-import android.provider.Settings;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.util.SimpleArrayMap;
 
@@ -93,32 +89,4 @@ public final class PermissionUtils {
         return false;
     }
 
-    /**
-     * Creates an {@link Intent} that redirects the user to the app's settings page.
-     *
-     * @param context Context used to access the app's package name
-     * @return An intent that, when used to {@link Activity#startActivity(Intent)}, will open the running app's Settings page.
-     * If no matching Activity could be found for the device's settings, this method returns null.
-     */
-    public static @Nullable Intent createAppSettingsIntent(Context context) {
-        Intent intent;
-        String packageName = context.getPackageName();
-        if (Build.VERSION.SDK_INT >= 9) {
-            // Utilize the dedicated Settings Action on API 9+
-            intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-            Uri uri = Uri.fromParts("package", packageName, null);
-            intent.setData(uri);
-
-        } else {
-            // Compatibility implementation using a View intent and the native Settings app
-            intent = new Intent(Intent.ACTION_VIEW);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.setClassName("com.android.settings", "com.android.settings.InstalledAppDetails");
-            String extraKey = Build.VERSION.SDK_INT == 8 ? "pkg" : "com.android.settings.ApplicationPkgName";
-            intent.putExtra(extraKey, packageName);
-        }
-
-        // If the intent can be used to resolve an Activity, return that intent. Otherwise, return null
-        return context.getPackageManager().resolveActivity(intent, 0) != null ? intent : null;
-    }
 }
