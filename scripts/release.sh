37480d997ea4077d0514a27734aabc6ad844358b
Shintaro Katafuchi <hotchemi@users.noreply.github.com>
2016-01-13 12:56:08 +0900

    Delete release.sh

--- scripts/release.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# You have to set $bintrayUser and $bintrayKey before run this script.
-set -eu
-
-./gradlew clean build generatePomFileForMavenPublication bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=true
-
-./gradlew clean build generatePomFileForMavenPublication bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey
