543078bf892229eee8a2961df47184cc4f6c8540
Shintaro Katafuchi <hotchemi@users.noreply.github.com>
2016-01-13 12:15:50 +0900

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,6 @@
 # You have to set $bintrayUser and $bintrayKey before run this script.
 set -eu
 
-../gradlew clean build generatePomFileForMavenPublication bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=true
+./gradlew clean build generatePomFileForMavenPublication bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=true
 
-../gradlew clean build generatePomFileForMavenPublication bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey
+./gradlew clean build generatePomFileForMavenPublication bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey
