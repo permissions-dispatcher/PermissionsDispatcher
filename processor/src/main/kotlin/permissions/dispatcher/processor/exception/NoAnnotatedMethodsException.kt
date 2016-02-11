a8a38b7a3c5a47e3bd35ad0506422e5c291ea503
Patrice Mueller <patrice.mueller@ti8m.ch>
2016-02-11 16:15:59 +0100

    Updte kotlin version.

--- processor/src/main/kotlin/permissions/dispatcher/processor/exception/NoAnnotatedMethodsException.kt
+++ processor/src/main/kotlin/permissions/dispatcher/processor/exception/NoAnnotatedMethodsException.kt
@@ -2,5 +2,5 @@ package permissions.dispatcher.processor.exception
 
 import permissions.dispatcher.processor.RuntimePermissionsElement
 
-public class NoAnnotatedMethodsException<A : Annotation>(rpe: RuntimePermissionsElement, type: Class<A>): RuntimeException("Annotated class '${rpe.inputClassName}' doesn't have any method annotated with '@${type.simpleName}'") {
+public class NoAnnotatedMethodsException(rpe: RuntimePermissionsElement, type: Class<*>): RuntimeException("Annotated class '${rpe.inputClassName}' doesn't have any method annotated with '@${type.simpleName}'") {
 }
--- processor/src/main/kotlin/permissions/dispatcher/processor/exception/PrivateMethodException.kt
+++ processor/src/main/kotlin/permissions/dispatcher/processor/exception/PrivateMethodException.kt
@@ -3,5 +3,5 @@ package permissions.dispatcher.processor.exception
 import permissions.dispatcher.processor.util.simpleString
 import javax.lang.model.element.ExecutableElement
 
-public class PrivateMethodException<A: Annotation>(e: ExecutableElement, annotationType: Class<A>): RuntimeException("Method '${e.simpleString()}()' annotated with '@${annotationType.simpleName}' must not be private") {
+class PrivateMethodException(e: ExecutableElement, annotationType: Class<*>): RuntimeException("Method '${e.simpleString()}()' annotated with '@${annotationType.simpleName}' must not be private") {
 }
