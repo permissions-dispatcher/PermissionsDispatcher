21e488662dc47950982734f8571fd01494088932
shiraji <isogai.shiraji@gmail.com>
2016-03-10 05:44:28 +0900

    Change to RetentionPolicy.CLASS
    This is a problem with kapt. Until they fix the issue with kapt, we need
    to make annotation policy "CLASS". For more detail please read
    https://github.com/hotchemi/PermissionsDispatcher/issues/112#issuecomment-194499063

--- library/src/main/java/permissions/dispatcher/NeedsPermission.java
+++ library/src/main/java/permissions/dispatcher/NeedsPermission.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * Register some methods which permissions are needed.
  */
 @Target(ElementType.METHOD)
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.CLASS)
 public @interface NeedsPermission {
     String[] value();
 }
