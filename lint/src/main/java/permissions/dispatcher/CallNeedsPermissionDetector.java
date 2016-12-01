96df7039a0ad7371e6f4736208b92709313391b6
Henry Addo <addhen@linuxmail.org>
2016-12-02 08:54:47 +0900

    Migrate from lombok API to Psi APIs
    - Bump lint library version
    - Migrate `CallNeedsPermissionDector` to the new Psi API
    - Migrate `NoCorrespondingNeedsPermissionDector` to the new Psi API
    - Add maven repository for needed depenencies for test compiles
    - Add test for the migrations
    - Show test status on the console. Useful on CI
    - Remove @VisibleOnTesting annotation detector default constructors.
    - Hand create mock classes in the test class instead of loading them
    from a separate package
    - Migrate `CallOnRequestPermissionsResultDetector` to the new Psi API
    - Upgrade android gradle plugin
    - Pull dependencies from `mavencentral`

--- lint/src/main/java/permissions/dispatcher/CallNeedsPermissionDetector.java
+++ lint/src/main/java/permissions/dispatcher/CallNeedsPermissionDetector.java
@@ -1,7 +1,6 @@
 package permissions.dispatcher;
 
 
-import com.android.tools.lint.client.api.JavaParser;
 import com.android.tools.lint.detector.api.Category;
 import com.android.tools.lint.detector.api.Detector;
 import com.android.tools.lint.detector.api.Implementation;
@@ -9,47 +8,74 @@ import com.android.tools.lint.detector.api.Issue;
 import com.android.tools.lint.detector.api.JavaContext;
 import com.android.tools.lint.detector.api.Scope;
 import com.android.tools.lint.detector.api.Severity;
+import com.intellij.psi.JavaElementVisitor;
+import com.intellij.psi.PsiAnnotation;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiMethodCallExpression;
+import com.intellij.psi.PsiReferenceExpression;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import lombok.ast.Annotation;
-import lombok.ast.AstVisitor;
-import lombok.ast.ClassDeclaration;
-import lombok.ast.ForwardingAstVisitor;
-import lombok.ast.MethodInvocation;
 
+public class CallNeedsPermissionDetector extends Detector implements Detector.JavaPsiScanner {
 
-public class CallNeedsPermissionDetector extends Detector implements Detector.JavaScanner {
     public static final Issue ISSUE = Issue.create("CallNeedsPermission",
             "Call the corresponding \"withCheck\" method of the generated PermissionsDispatcher class instead",
             "Directly invoking a method annotated with @NeedsPermission may lead to misleading behaviour on devices running Marshmallow and up. Therefore, it is advised to use the generated PermissionsDispatcher class instead, which provides a \"withCheck\" method that safely handles runtime permissions.",
             Category.CORRECTNESS,
             7,
             Severity.ERROR,
-            new Implementation(CallNeedsPermissionDetector.class, EnumSet.of(Scope.ALL_JAVA_FILES)));
+            new Implementation(CallNeedsPermissionDetector.class,
+                    EnumSet.of(Scope.ALL_JAVA_FILES)));
 
     static List<String> generatedClassNames = new ArrayList<String>();
 
+    static List<String> methods = Collections.emptyList();
+
     @Override
-    public AstVisitor createJavaVisitor(JavaContext context) {
+    public List<Class<? extends PsiElement>> getApplicablePsiTypes() {
+        return Collections.<Class<? extends PsiElement>>singletonList(PsiClass.class);
+    }
+
+    public CallNeedsPermissionDetector() {
+        // No-op
+    }
+
+    @Override
+    public JavaElementVisitor createPsiVisitor(final JavaContext context) {
         if (context.getPhase() == 1) {
             // find out which class has RuntimePermissions
             return new AnnotationChecker(context);
-        } else if (context.getPhase() == 2) {
-            // find out which class call method with NeedPermission
-            // exclude class with above with name XxxPermissionsDispatcher in the same package.
-            return new MethodCallChecker(context);
         }
         return null;
     }
 
-    private static class AnnotationChecker extends ForwardingAstVisitor {
+    @Override
+    public List<String> getApplicableMethodNames() {
+        return methods;
+    }
+
+    @Override
+    public void visitMethod(JavaContext context, JavaElementVisitor visitor,
+            PsiMethodCallExpression node, PsiMethod method) {
+        if (methods.contains(method.getName())) {
+            context.report(ISSUE, node, context.getLocation(node),
+                    "Trying to access permission-protected method directly");
+        }
+    }
+
+    private static class AnnotationChecker extends JavaElementVisitor {
+
         private final JavaContext context;
-        private Set<String> matchingAnnotationTypeNames;
+
+        private final Set<String> matchingAnnotationTypeNames;
 
         private AnnotationChecker(JavaContext context) {
             this.context = context;
@@ -60,53 +86,43 @@ public class CallNeedsPermissionDetector extends Detector implements Detector.Ja
         }
 
         @Override
-        public boolean visitAnnotation(Annotation node) {
+        public void visitReferenceExpression(PsiReferenceExpression expression) {
+            skipGeneratedFiles(context);
+            super.visitReferenceExpression(expression);
+        }
+
+        @Override
+        public void visitAnnotation(PsiAnnotation annotation) {
             if (!context.isEnabled(ISSUE)) {
-                return super.visitAnnotation(node);
+                super.visitAnnotation(annotation);
+                return;
             }
 
-            String type = node.astAnnotationTypeReference().getTypeName();
+            String type = annotation.getQualifiedName();
             if (!matchingAnnotationTypeNames.contains(type)) {
-                return super.visitAnnotation(node);
+                super.visitAnnotation(annotation);
+                return;
             }
 
-            JavaParser.ResolvedNode resolvedNode = context.resolve(node.getParent());
-            if (resolvedNode instanceof JavaParser.ResolvedClass) {
-                generatedClassNames.add(resolvedNode.getName() + "PermissionsDispatcher");
+            PsiClass[] classes = context.getJavaFile().getClasses();
+            if (classes.length > 0 && classes[0].getName() != null) {
+                generatedClassNames.add(classes[0].getName() + "PermissionsDispatcher");
                 // let's check method call!
-                context.requestRepeat(new CallNeedsPermissionDetector(), EnumSet.of(Scope.ALL_JAVA_FILES));
+                context.requestRepeat(new CallNeedsPermissionDetector(),
+                        EnumSet.of(Scope.ALL_JAVA_FILES));
             }
-            return super.visitAnnotation(node);
-        }
-    }
-
-    private class MethodCallChecker extends ForwardingAstVisitor {
-        JavaContext javaContext;
-
-        public MethodCallChecker(JavaContext context) {
-            javaContext = context;
+            super.visitAnnotation(annotation);
         }
 
-        @Override
-        public boolean visitClassDeclaration(ClassDeclaration node) {
-            // Ignore a class that is generated by PermissionsDispatcher
-            return generatedClassNames.contains(javaContext.resolve(node).getName());
-        }
-
-        @Override
-        public boolean visitMethodInvocation(MethodInvocation node) {
-            JavaParser.ResolvedNode resolved = javaContext.resolve(node);
-            if (!(resolved instanceof JavaParser.ResolvedMethod)) {
-                return super.visitMethodInvocation(node);
+        private static void skipGeneratedFiles(JavaContext context) {
+            PsiClass[] classes = context.getJavaFile().getClasses();
+            if (classes.length > 0 && classes[0].getName() != null) {
+                String qualifiedName = classes[0].getName();
+                if (qualifiedName.contains("PermissionsDispatcher")) {
+                    // skip generated files
+                    return;
+                }
             }
-            JavaParser.ResolvedMethod method = (JavaParser.ResolvedMethod) resolved;
-            JavaParser.ResolvedAnnotation annotation = method.getAnnotation("permissions.dispatcher.NeedsPermission");
-            if (annotation == null) {
-                return super.visitMethodInvocation(node);
-            }
-
-            javaContext.report(ISSUE, javaContext.getLocation(node), "Trying to access permission-protected method directly");
-            return super.visitMethodInvocation(node);
         }
     }
 }
---
+++ lint/src/test/java/permissions/dispatcher/CallOnRequestPermissionsResultDetectorTest.java
@@ -0,0 +1,125 @@
+package permissions.dispatcher;
+
+import com.google.common.collect.ImmutableList;
+
+import com.android.tools.lint.detector.api.Detector;
+import com.android.tools.lint.detector.api.Issue;
+
+import org.intellij.lang.annotations.Language;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author Henry Addo
+ */
+public class CallOnRequestPermissionsResultDetectorTest extends BaseLintDetectorTest {
+
+    @Override
+    protected Detector getDetector() {
+        return new CallOnRequestPermissionsResultDetector();
+    }
+
+    @Override
+    protected List<Issue> getIssues() {
+        return ImmutableList.of(CallOnRequestPermissionsResultDetector.ISSUE);
+
+    }
+
+    @Test
+    public void testCallOnRequestPermissionsResultDetectorNoError() throws Exception {
+        @Language("JAVA") String runtimePerms = getRuntimePermission();
+
+        @Language("JAVA") String onNeeds = getOnNeedsPermission();
+
+        String onShow = getOnRationaleAnnotation();
+
+        @Language("JAVA") String foo = ""
+                + "package permissions.dispatcher;\n"
+                + "@RuntimePermissions\n"
+                + "public class Foo {\n"
+                + "public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {\n"
+                + "FooPermissionsDispatcher.onRequestPermissionsResult();\n"
+                + "}\n"
+                + "@NeedsPermission(\"Camera\")"
+                + "public void showCamera() {"
+                + "}\n"
+                + "@OnShowRationale(\"Camera\")"
+                + "public void someMethod() {"
+                + "}\n"
+                + "}";
+
+        @Language("JAVA") String generatedClass = ""
+                + "package permissions.dispatcher;\n"
+                + "public class FooPermissionsDispatcher {\n"
+                + "public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {\n"
+                + "}\n"
+                + "public static void onRequestPermissionsResult() {"
+                + "}\n"
+                + "}";
+
+        String result = lintProject(
+                java("src/permissions/dispatcher/RuntimePermissions.java",
+                        runtimePerms.toString()),
+                java("src/permissions/dispatcher/NeedsPermission.java", onNeeds),
+                java("src/permissions/dispatcher/OnShowRationale.java", onShow),
+                java("src/permissions/dispatcher/FooPermissionsDispatcher.java",
+                        generatedClass),
+                java("src/permissions/dispatcher/Foo.java", foo));
+
+        assertEquals(result, "No warnings.");
+    }
+
+    @Test
+    public void testCallOnRequestPermissionsResultDetector() throws Exception {
+
+        String runtimePerms = getRuntimePermission();
+
+        String onNeeds = getOnNeedsPermission();
+
+        String onShow = getOnRationaleAnnotation();
+
+        @Language("JAVA") String foo = ""
+                + "package permissions.dispatcher;\n"
+                + "@RuntimePermissions\n"
+                + "public class Foo {\n"
+                + "public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {\n"
+                + "}\n"
+                + "@NeedsPermission(\"Camera\")"
+                + "public void showCamera() {"
+                + "}\n"
+                + "@OnShowRationale(\"Camera\")"
+                + "public void someMethod() {"
+                + "}\n"
+                + "}";
+
+        String result = lintProject(
+                java(SOURCE_PATH + "RuntimePermissions.java",
+                        runtimePerms.toString()),
+                java(SOURCE_PATH + "NeedsPermission.java", onNeeds),
+                java(SOURCE_PATH + "OnShowRationale.java", onShow),
+                java(SOURCE_PATH + "Foo.java", foo));
+
+        String error = ""
+                + SOURCE_PATH + "Foo.java:4: Error: {\n"
+                + "Generated onRequestPermissionsResult method not called [NeedOnRequestPermissionsResult]\n"
+                + "public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {\n"
+                + "^\n"
+                + "1 errors, 0 warnings\n";
+        assertEquals(result, error);
+    }
+
+    private String getRuntimePermission() {
+        @Language("JAVA") String runPermissions = ""
+                + "package permissions.dispatcher;\n"
+                + "import java.lang.annotation.ElementType;\n"
+                + "import java.lang.annotation.Retention;\n"
+                + "import java.lang.annotation.RetentionPolicy;\n"
+                + "import java.lang.annotation.Target;\n"
+                + "@Target(ElementType.TYPE)\n"
+                + "@Retention(RetentionPolicy.CLASS)\n"
+                + "public @interface RuntimePermissions {\n"
+                + "}";
+        return runPermissions;
+    }
+}
