e1467dc0915766d657039334200dfd36b9c762e1
hotchemi <hot.chemistry21@gmail.com>
2017-05-01 15:51:23 +0900

    Prepare for next release. Move sh files to scripts dir.

--- release.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-./gradlew clean build generatePomFileForMavenPublication :processor:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
-./gradlew :library:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
---
+++ scripts/release.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+../gradlew clean build generatePomFileForMavenPublication :processor:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
+../gradlew :library:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
