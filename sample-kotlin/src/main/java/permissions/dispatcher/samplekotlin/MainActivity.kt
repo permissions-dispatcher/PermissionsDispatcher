638e1a4c94bbf59e2f693aac2ef7fc5705573c49
Aleksey Bogomolov <horse315@gmail.com>
2017-06-23 10:55:55 +0300

    kotlin sample module

---
+++ sample-kotlin/src/main/java/permissions/dispatcher/samplekotlin/MainActivity.kt
@@ -0,0 +1,102 @@
+package permissions.dispatcher.samplekotlin
+
+import android.Manifest
+import android.os.Bundle
+import android.support.annotation.StringRes
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import permissions.dispatcher.*
+import permissions.dispatcher.samplekotlin.camera.CameraPreviewFragment
+import permissions.dispatcher.samplekotlin.contacts.ContactsFragment
+
+@RuntimePermissions
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById(R.id.button_camera).setOnClickListener(this)
+        findViewById(R.id.button_contacts).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.button_camera -> {
+                // NOTE: delegate the permission handling to generated method
+                MainActivityPermissionsDispatcher.showCameraWithCheck(this)
+            }
+
+            R.id.button_contacts -> {
+                // NOTE: delegate the permission handling to generated method
+                MainActivityPermissionsDispatcher.showContactsWithCheck(this)
+            }
+
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // NOTE: delegate the permission handling to generated method
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults)
+    }
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    fun showCamera() {
+        // NOTE: Perform action that requires the permission. If this is run by PermissionsDispatcher, the permission will have been granted
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.sample_content_fragment, CameraPreviewFragment.newInstance())
+                .addToBackStack("camera")
+                .commitAllowingStateLoss()
+    }
+
+    @NeedsPermission(Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS)
+    fun showContacts() {
+        // NOTE: Perform action that requires the permission.
+        // If this is run by PermissionsDispatcher, the permission will have been granted
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.sample_content_fragment, ContactsFragment.newInstance())
+                .addToBackStack("contacts")
+                .commitAllowingStateLoss()
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    fun showRationaleForCamera(request: PermissionRequest) {
+        // NOTE: Show a rationale to explain why the permission is needed, e.g. with a dialog.
+        // Call proceed() or cancel() on the provided PermissionRequest to continue or abort
+        showRationaleDialog(R.string.permission_camera_rationale, request)
+    }
+
+    @OnShowRationale(Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS)
+    fun showRationaleForContact(request: PermissionRequest) {
+        // NOTE: Show a rationale to explain why the permission is needed, e.g. with a dialog.
+        // Call proceed() or cancel() on the provided PermissionRequest to continue or abort
+        showRationaleDialog(R.string.permission_contacts_rationale, request)
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    fun onCameraDenied() {
+        // NOTE: Deal with a denied permission, e.g. by showing specific UI
+        // or disabling certain functionality
+        Toast.makeText(this, R.string.permission_camera_denied, Toast.LENGTH_SHORT).show()
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    fun onCameraNeverAskAgain() {
+        Toast.makeText(this, R.string.permission_camera_never_askagain, Toast.LENGTH_SHORT).show()
+    }
+
+    fun onBackClick(view: View) {
+        supportFragmentManager.popBackStack()
+    }
+
+    private fun showRationaleDialog(@StringRes messageResId: Int, request: PermissionRequest) {
+        AlertDialog.Builder(this)
+                .setPositiveButton(R.string.button_allow) { dialog, which -> request.proceed() }
+                .setNegativeButton(R.string.button_deny) { dialog, which -> request.cancel() }
+                .setCancelable(false)
+                .setMessage(messageResId)
+                .show()
+    }
+}
---
+++ sample-kotlin/src/main/java/permissions/dispatcher/samplekotlin/camera/CameraPreviewFragment.java
@@ -0,0 +1,135 @@
+/*
+* Copyright 2015 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package permissions.dispatcher.samplekotlin.camera;
+
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import permissions.dispatcher.samplekotlin.R;
+
+
+/**
+ * Displays a {@link CameraPreview} of the first {@link Camera}.
+ * An error message is displayed if the Camera is not available.
+ * <p>
+ * This Fragment is only used to illustrate that access to the Camera API has been granted (or
+ * denied) as part of the runtime permissions model. It is not relevant for the use of the
+ * permissions API.
+ * <p>
+ * Implementation is based directly on the documentation at
+ * http://developer.android.com/guide/topics/media/camera.html
+ */
+public class CameraPreviewFragment extends Fragment {
+
+    private static final String TAG = "CameraPreview";
+
+    /**
+     * Id of the camera to access. 0 is the first camera.
+     */
+    private static final int CAMERA_ID = 0;
+
+    private CameraPreview mPreview;
+    private Camera mCamera;
+
+    public static CameraPreviewFragment newInstance() {
+        return new CameraPreviewFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_camera, null);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initCamera();
+    }
+
+    private void initCamera() {
+        mCamera = getCameraInstance(CAMERA_ID);
+        Camera.CameraInfo cameraInfo = null;
+
+        if (mCamera != null) {
+            // Get camera info only if the camera is available
+            cameraInfo = new Camera.CameraInfo();
+            Camera.getCameraInfo(CAMERA_ID, cameraInfo);
+        }
+
+        // Get the rotation of the screen to adjust the preview image accordingly.
+        final int displayRotation = getActivity().getWindowManager().getDefaultDisplay()
+                .getRotation();
+
+        if (getView() == null) {
+            return;
+        }
+
+        FrameLayout preview = (FrameLayout) getView().findViewById(R.id.camera_preview);
+        preview.removeAllViews();
+
+        if (mPreview == null) {
+            // Create the Preview view and set it as the content of this Activity.
+            mPreview = new CameraPreview(getActivity(), mCamera, cameraInfo, displayRotation);
+        } else {
+            mPreview.setCamera(mCamera, cameraInfo, displayRotation);
+        }
+
+        preview.addView(mPreview);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mCamera == null) {
+            initCamera();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop camera access
+        releaseCamera();
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(int cameraId) {
+        Camera c = null;
+        try {
+            c = Camera.open(cameraId); // attempt to get a Camera instance
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+            Log.d(TAG, "Camera " + cameraId + " is not available: " + e.getMessage());
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
+}
