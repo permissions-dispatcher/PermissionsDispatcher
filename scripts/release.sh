0927de34c95bfbb2497e1fca520bd672466a33ee
hotchemi <hot.chemistry21@gmail.com>
2017-12-19 04:03:17 +0900

    Fix release script a bit.

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-../gradlew clean build generatePomFileForMavenPublication :processor:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
-../gradlew :library:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
+./gradlew clean build generatePomFileForMavenPublication :processor:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
+./gradlew :library:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
