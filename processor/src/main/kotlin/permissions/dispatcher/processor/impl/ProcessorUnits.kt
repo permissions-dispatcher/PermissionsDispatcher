6b4bbfe078d968372909bcadebb1141946af5526
hotchemi <hot.chemistry21@gmail.com>
2018-07-10 02:23:43 +0900

    Remove native Fragment support.
    In favor of Android P.

--- processor/src/main/kotlin/permissions/dispatcher/processor/impl/ProcessorUnits.kt
+++ processor/src/main/kotlin/permissions/dispatcher/processor/impl/ProcessorUnits.kt
@@ -1,19 +1,15 @@
 package permissions.dispatcher.processor.impl
 
 import permissions.dispatcher.processor.impl.java.JavaActivityProcessorUnit
-import permissions.dispatcher.processor.impl.java.JavaNativeFragmentProcessorUnit
-import permissions.dispatcher.processor.impl.java.JavaSupportFragmentProcessorUnit
+import permissions.dispatcher.processor.impl.java.JavaFragmentProcessorUnit
 import permissions.dispatcher.processor.impl.kotlin.KotlinActivityProcessorUnit
-import permissions.dispatcher.processor.impl.kotlin.KotlinNativeFragmentProcessorUnit
-import permissions.dispatcher.processor.impl.kotlin.KotlinSupportFragmentProcessorUnit
+import permissions.dispatcher.processor.impl.kotlin.KotlinFragmentProcessorUnit
 import javax.annotation.processing.Messager
 
 fun javaProcessorUnits(messager: Messager) = listOf(
         JavaActivityProcessorUnit(messager),
-        JavaSupportFragmentProcessorUnit(messager),
-        JavaNativeFragmentProcessorUnit(messager))
+        JavaFragmentProcessorUnit(messager))
 
 fun kotlinProcessorUnits(messager: Messager) = listOf(
         KotlinActivityProcessorUnit(messager),
-        KotlinSupportFragmentProcessorUnit(messager),
-        KotlinNativeFragmentProcessorUnit(messager))
+        KotlinFragmentProcessorUnit(messager))
