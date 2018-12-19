4a3b68556a80f515337157852f12dfc060b20d9e
hotchemi <hot.chemistry21@gmail.com>
2018-12-20 04:44:20 +0900

    Add travisci setting.

--- scripts/push-to-github-io.sh
+++ scripts/push-to-github-io.sh
@@ -3,35 +3,33 @@ set -eu
 
 declare repoName="permissions-dispatcher.github.io"
 declare repoUrl="git@github.com:permissions-dispatcher/${repoName}.git"
-declare commitMessage="Update content from TravisCI."
 declare distDir="_book"
-
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-  echo "Skip the job as current branch is not master."
-  exit 0;
-fi
+declare branch="master"
+declare commitMessage="Update content from TravisCI."
 
 commit_and_push_changes() {
-    git config --global user.email "$GH_USER_EMAIL" \
-        && git config --global user.name "$GH_USER_NAME" \
-        && git init \
-        && git add -A \
-        && git commit --message "$2" \
-        && git push --quiet --force "${repoUrl}" "$1"
+  git config --global user.email "$GH_USER_EMAIL" \
+    && git config --global user.name "$GH_USER_NAME" \
+    && git add -A \
+    && git commit --message "$2" \
+    && git push --quiet --force "${repoUrl}" "$1"
 }
 
+# if [ "$TRAVIS_BRANCH" != "master" ] || [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+#   echo "Skip updating gitbook: branch is not master or the build was triggered by pull request."
+#   exit 0;
+# fi
+
 echo "Clone repo"
-rm -rf "${repoName}"
-mkdir "${repoName}"
+rm -rf ${repoName}
 git clone ${repoUrl} ${repoName}
-echo "Remove files"
-find "${repoName}" -type f -delete 
-echo "Install dependencies"
-yarn global add gitbook-cli
-echo "Update content"
+echo "Install Node and dependencies"
+nvm install 9.5.0
+npm install -g gitbook-cli
+echo "Build HTML"
 gitbook build
 echo "Copy files"
 cp -Rf "${distDir}/doc"* "${distDir}/gitbook"* "${distDir}/index.html" "${distDir}/search_index.json" "${repoName}"
 echo "Commit and push"
 cd "${repoName}"
-commit_and_push_changes  "master" "${commitMessage}"
+commit_and_push_changes "${branch}" "${commitMessage}"
