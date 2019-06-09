9aa1fcc9bc3e96fb588bed11b41d5d1919fa05b4
hotchemi <shintaro.katafuchi@gmail.com>
2019-06-09 18:04:36 +0900

    Fix to use old mockito for test module.

--- processor/src/test/java/permissions/dispatcher/processor/util/ExtensionsTest.kt
+++ processor/src/test/java/permissions/dispatcher/processor/util/ExtensionsTest.kt
@@ -5,13 +5,13 @@ import com.squareup.kotlinpoet.ParameterizedTypeName
 import com.squareup.kotlinpoet.TypeName
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.mockito.Mockito
 import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
 class ExtensionsTest {
     @Test
     fun `java Byte is converted into kotlin Byte`() {
-        val typeName = Mockito.mock(TypeName::class.java)
+        val typeName = mock(TypeName::class.java)
         `when`(typeName.toString()).thenReturn("java.lang.Byte")
         val expected = typeName.correctJavaTypeToKotlinType().toString()
         assertEquals(expected, "kotlin.Byte")
@@ -19,7 +19,7 @@ class ExtensionsTest {
 
     @Test
     fun `java Double is converted into kotlin Double`() {
-        val typeName = Mockito.mock(TypeName::class.java)
+        val typeName = mock(TypeName::class.java)
         `when`(typeName.toString()).thenReturn("java.lang.Double")
         val expected = typeName.correctJavaTypeToKotlinType().toString()
         assertEquals(expected, "kotlin.Double")
@@ -27,7 +27,7 @@ class ExtensionsTest {
 
     @Test
     fun `java Object is converted into kotlin Any`() {
-        val typeName = Mockito.mock(TypeName::class.java)
+        val typeName = mock(TypeName::class.java)
         `when`(typeName.toString()).thenReturn("java.lang.Object")
         val expected = typeName.correctJavaTypeToKotlinType().toString()
         assertEquals(expected, "kotlin.Any")
@@ -35,7 +35,7 @@ class ExtensionsTest {
 
     @Test
     fun `java String is converted into kotlin String`() {
-        val typeName = Mockito.mock(TypeName::class.java)
+        val typeName = mock(TypeName::class.java)
         `when`(typeName.toString()).thenReturn("java.lang.String")
         val expected = typeName.correctJavaTypeToKotlinType().toString()
         assertEquals(expected, "kotlin.String")
@@ -43,8 +43,8 @@ class ExtensionsTest {
 
     @Test
     fun `java String in List is converted into kotlin String`() {
-        val parameterizedTypeName = Mockito.mock(ParameterizedTypeName::class.java)
-        val typeName = Mockito.mock(TypeName::class.java)
+        val parameterizedTypeName = mock(ParameterizedTypeName::class.java)
+        val typeName = mock(TypeName::class.java)
         `when`(typeName.toString()).thenReturn("java.lang.String")
         `when`(parameterizedTypeName.rawType).thenReturn(ClassName.bestGuess("kotlin.collections.List"))
         `when`(parameterizedTypeName.typeArguments).thenReturn(listOf(typeName))
