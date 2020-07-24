2b125d49027de3c6b97f4aa80cfa8bb5b4bb8b86
hotchemi <shintaro.katafuchi@smartnews.com>
2020-07-24 20:35:28 +0900

    fix: use int literal directly

--- ktx/src/main/java/permissions/dispatcher/ktx/LocationPermission.kt
+++ ktx/src/main/java/permissions/dispatcher/ktx/LocationPermission.kt
@@ -10,11 +10,11 @@ import android.os.Build
  * **See:** [Android Developers](https://developer.android.com/training/location/permissions)
  */
 enum class LocationPermission(internal val permission: String, internal val apiLevel: Int) {
-    FINE(Manifest.permission.ACCESS_FINE_LOCATION, Build.VERSION_CODES.BASE),
-    COARSE(Manifest.permission.ACCESS_COARSE_LOCATION, Build.VERSION_CODES.BASE),
+    FINE(Manifest.permission.ACCESS_FINE_LOCATION, 1),
+    COARSE(Manifest.permission.ACCESS_COARSE_LOCATION, 1),
 
     @SuppressLint("InlinedApi")
-    BACKGROUND(Manifest.permission.ACCESS_BACKGROUND_LOCATION, Build.VERSION_CODES.Q)
+    BACKGROUND(Manifest.permission.ACCESS_BACKGROUND_LOCATION, 29)
 }
 
 internal fun Array<out LocationPermission>.filterByApiLevel(sdkVer: Int = Build.VERSION.SDK_INT) =
