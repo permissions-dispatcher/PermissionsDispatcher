66da222d604c5e274a4a82dacaa82d060500e381
Shintaro Katafuchi <hotchemi@users.noreply.github.com>
2020-08-30 21:13:47 +0900

    fix: support namedMethodReference (#682)
    * fix: support namedMethodReference
    
    * fix: add one more test case

--- lint/src/test/java/permissions/dispatcher/CallOnRequestPermissionsResultDetectorKtTest.kt
+++ lint/src/test/java/permissions/dispatcher/CallOnRequestPermissionsResultDetectorKtTest.kt
@@ -1,17 +1,15 @@
 package permissions.dispatcher
 
-import org.intellij.lang.annotations.Language
-import org.junit.Test
-
 import com.android.tools.lint.checks.infrastructure.TestFiles.java
 import com.android.tools.lint.checks.infrastructure.TestFiles.kt
 import com.android.tools.lint.checks.infrastructure.TestLintTask.lint
+import org.intellij.lang.annotations.Language
+import org.junit.Test
 import permissions.dispatcher.Utils.onNeedsPermission
 import permissions.dispatcher.Utils.onRationaleAnnotation
 import permissions.dispatcher.Utils.runtimePermission
 
 class CallOnRequestPermissionsResultDetectorKtTest {
-
     @Test
     @Throws(Exception::class)
     fun callOnRequestPermissionsResultDetectorNoErrorForKotlin() {
@@ -20,7 +18,7 @@ class CallOnRequestPermissionsResultDetectorKtTest {
 
                 @RuntimePermissions
                 class Foo : android.app.Activity {
-                    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+                    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
                         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
                         onRequestPermissionsResult(requestCode, grantResults)
                     }
@@ -52,6 +50,94 @@ class CallOnRequestPermissionsResultDetectorKtTest {
                 .issues(CallOnRequestPermissionsResultDetector.ISSUE)
                 .run()
                 .expectClean()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun callOnRequestPermissionsResultDetectorNoErrorWithNamedMethodReference() {
+        @Language("kotlin") val foo = """
+                package permissions.dispatcher
+
+                @RuntimePermissions
+                class Foo : android.app.Activity {
+                    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) =
+                        onRequestPermissionsResult(requestCode, grantResults)
+
+                    @NeedsPermission("Camera")
+                    fun showCamera() {
+                    }
+
+                    @OnShowRationale("Camera")
+                    fun someMethod() {
+                    }
+                }
+                """.trimMargin()
+
+        @Language("kotlin") val generatedClass = """
+                package permissions.dispatcher
+
+                fun Foo.onRequestPermissionsResult(requestCode: Int, grantResults: IntArray) {
+                }
+                """.trimMargin()
+
+        lint()
+            .files(
+                java(runtimePermission),
+                java(onNeedsPermission),
+                java(onRationaleAnnotation),
+                kt(generatedClass),
+                kt(foo))
+            .issues(CallOnRequestPermissionsResultDetector.ISSUE)
+            .run()
+            .expectClean()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun callOnRequestPermissionsResultDetectorErrorWithWrongMethod() {
+        @Language("kotlin") val foo = """
+                package permissions.dispatcher
+
+                @RuntimePermissions
+                class Foo : android.app.Activity {
+                    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) =
+                        hoge(requestCode, grantResults)
+
+                    @NeedsPermission("Camera")
+                    fun showCamera() {
+                    }
+
+                    @OnShowRationale("Camera")
+                    fun someMethod() {
+                    }
+                }
+                """.trimMargin()
+
+        @Language("kotlin") val generatedClass = """
+                package permissions.dispatcher
+
+                fun Foo.hoge(requestCode: Int, grantResults: IntArray) {
+                }
+                """.trimMargin()
+
+        val expectedText = """
+                |src/permissions/dispatcher/Foo.kt:5: Error: Generated onRequestPermissionsResult method not called [NeedOnRequestPermissionsResult]
+                |                    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) =
+                |                    ^
+                |1 errors, 0 warnings
+                """.trimMargin()
+
+        lint()
+            .files(
+                java(runtimePermission),
+                java(onNeedsPermission),
+                java(onRationaleAnnotation),
+                kt(foo))
+            .issues(CallOnRequestPermissionsResultDetector.ISSUE)
+            .run()
+            .expect(expectedText)
+            .expectErrorCount(1)
+            .expectWarningCount(0)
     }
 
     @Test
